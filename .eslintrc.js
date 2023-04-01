d157ab2b377e746c6176601247a84c9584909e4e
vnhanh <nghiamy15@gmail.com>
2023-04-02 02:16:33 +0700

    fix: resolve typescript eslint issue
    move redux-logger to devDependencies section in package.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,15 @@ module.exports = {
   root: true,
   extends: [
     '@react-native-community/eslint-config', // Default RN config
-    'standard-with-typescript', // Installed in step 2
-    'eslint-config-prettier' // Installed in step 3
+    'eslint-config-prettier',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended'
   ],
-  parser: '@typescript-eslint/parser', // Installed in step 2
+  parser: '@typescript-eslint/parser',
   plugins: [
-    '@typescript-eslint', // Installed in step 2
-    'react', // Installed in step 1
-    'react-native' // Installed in step 1
+    '@typescript-eslint',
+    'react',
+    'react-native'
   ],
   'parserOptions': {
     'ecmaFeatures': {
@@ -52,5 +53,4 @@ module.exports = {
     // Indent props with 2 spaces
     'react/jsx-indent-props': ['error', 2]
   }
-
 }
